8852badd3442643ed9c4086eb767c6812b939114
Andreas Brodersen <apb@magenta-aps.dk>
2024-11-14 15:22:57 +0100

    feat: add working nn spam detection and a prediction endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 from fastapi.responses import RedirectResponse
+
 from fastapi import FastAPI
-from .models.nn import Net
+from tensorflow.keras.saving import load_model
+import numpy as np
+from tf_keras.preprocessing.sequence import pad_sequences
+from tf_keras.preprocessing.text import Tokenizer
+
+try:
+    # Load the saved model and tokenizer
+    loaded_model = load_model("models/spam_detection_model.h5")
+    tokenizer = Tokenizer()
+    tokenizer.word_index = np.load(
+        "models/tokenizer_word_index.npy", allow_pickle=True
+    ).item()
+except Exception as e:
+    print("This probably means that you need to train the model")
+    print("Error:", e)
+
 
 app = FastAPI()
 
@@ -10,20 +26,42 @@ async def docs_redirect():
     return RedirectResponse(url="/docs")
 
 
-@app.get("/predict")
-def predict(header: str, context: str):
-    # TODO: Get prediction from a model
-    return {"result": "50% sure there's something here 🤷"}
+@app.post("/predict")
+async def predict(text: str):
+    # Tokenize the input text
+    sequence = tokenizer.texts_to_sequences([text])[0]
+
+    # Pad the sequence to match the model's input length
+    # TODO: better way of setting the longest_sequence up
+    # longest_sequence if len(sequence) <= longest_sequence else len(sequence)
+    # the hardcoded 23 is from looking at logs during training
+    padded_sequence = pad_sequences(
+        [sequence],
+        maxlen=(23 if len(sequence) <= 23 else len(sequence)),
+        padding="post",
+    )
+
+    prediction = loaded_model.predict(padded_sequence)
+
+    # Convert prediction to binary classification (spam or not spam)
+    predicted_class = np.round(prediction[0]).astype(int)[0]
+
+    # Create response
+    if predicted_class == 1:
+        result = f"The text '{text}' is classified as SPAM."
+    else:
+        result = f"The text '{text}' is classified as HAM."
+
+    print("\n\nprediction:", prediction)
+    return {"prediction": result, "confidence": float(round(prediction[0][0], 2))}
 
 
 @app.post("/train")
-def train(header: str, context: str, handling: str):
-    # TODO: Tokenize inputs and send to a model
-    return {"result": "The moel has been improved and is now better than ever! 🦄"}
+async def train(type: str, header: str, context: str, handling: str):
+    # TODO: Tokenize inputs and send to a model for the type of match
+    return {"result": "The model has been improved and is now better than ever! 🦄"}
 
 
 @app.get("/experiment")
-def experiment():
-    net = Net()
-    print(net)
-    return net
+async def experiment():
+    print("experiment")
